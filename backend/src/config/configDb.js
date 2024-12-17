a9233123a8bfcfed47ac00515cdf9ae0d4152c5e
cataldo-lab <benjamin.cataldo1901@alumnos.ubiobio.cl>
2024-12-17 18:29:46 -0300

    Nuevo initialSetup

--- backend/src/config/configDb.js
+++ backend/src/config/configDb.js
@@ -10,8 +10,8 @@ export const AppDataSource = new DataSource({
   password: `${PASSWORD}`,
   database: `${DATABASE}`,
   entities: ["src/entity/**/*.js"],
-  synchronize: false,
-  logging: true,
+  synchronize: true,
+  logging: false,
 });
 
 export async function connectDB() {
